616aadbaace052f879247c0d677d4976c41cc8d2
andyka adi <andykaadi23@gmail.com>
2023-07-31 10:04:16 +0700

    import list

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
+import data from "./data";
+
 const App = () => {
+  const [people, setPeople] = useState(data);
+  console.log(data);
   return <h2>Birthday Reminder - Starter</h2>;
 };
 export default App;
